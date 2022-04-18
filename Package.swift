108672da22b9b7cdd3ceb29d900c33a0af4c5553
dclife <dclife2009@gmail.com>
2022-04-18 15:11:31 +0200

    update SPM

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "WhatsNew",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(name: "WhatsNew", targets: ["WhatsNew"]),
+    ],
+    targets: [
+        .target(name: "WhatsNew", path: "WhatsNew")
+    ]
+)
